bbd5bccbd88fb32aa836e9fc8c0873387b0e41bc
gfranciscoerazom <gfranciscoerazom@protonmail.com>
2021-07-23 12:24:40 -0500

    Declaración de variables

--- enums.c
+++ enums.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+int i, j, k;
+unsigned short minimunMinutes = 30;
+unsigned char y = 'y';
+unsigned char dailyWorkedHours = 0;
+
 enum weekDays {Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday} today;
 
 /*enum NOMBRE-ENUM {
---
+++ variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Variable declaration
+int a, b, c;
+float f, g, h;
+
+int main(){
+    // Variable initialization.
+    a = 1;
+    b = 34;
+    c = a + b;
+
+    printf("Value of sume is: %d", c);
+    return 0;
+}
